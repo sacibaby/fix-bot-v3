8823c5824f3c8d435c31888d1e4a5c71ee9312e6
sacibaby <111527520+sacibaby@users.noreply.github.com>
2022-10-16 19:17:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -68,9 +68,9 @@ global.psaweria = 'https://saweria.com/mrw4h1d'
 
 /*============== NOMOR ==============*/
 global.nomorbot = '62XXXXXXXXXXX'
-global.nomorown = '6282127487538'
+global.nomorown = '64770378874'
 global.namebot = 'WH-MODS-BOT-V1'
-global.nameown = 'WH MODS DEV'
+global.nameown = '📍SUNETH📍'
 
 /*============== Github Thanks To ==============*/
 global.namagithub = 'Nama Github Belum Diisi' // nama github lu
